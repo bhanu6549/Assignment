026719a684fc67f4eeb3e2d59f21b803b23a4558
erpujabaidya <erpujabaidya@gmail.com>
2020-03-20 12:11:09 +0530

    Initiated By Puja

---
+++ KalingaCampusProject/src/main/java/com/mindtree/kalingacampusproject/exception/serviceexception/customexception/LeadAlreadyPresentException.java
@@ -0,0 +1,31 @@
+package com.mindtree.kalingacampusproject.exception.serviceexception.customexception;
+
+import com.mindtree.kalingacampusproject.exception.serviceexception.KalingaCampusServiceApplicationException;
+
+public class LeadAlreadyPresentException extends KalingaCampusServiceApplicationException {
+
+	public LeadAlreadyPresentException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public LeadAlreadyPresentException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LeadAlreadyPresentException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LeadAlreadyPresentException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LeadAlreadyPresentException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+}
