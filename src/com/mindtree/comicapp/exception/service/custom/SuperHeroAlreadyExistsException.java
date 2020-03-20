c299f06ed0b338cacdcff20ee2e16d64ef801338
Naitik1998 <naitik.rana1998@gmail.com>
2020-03-20 11:49:30 +0530

    Comic App is commit

---
+++ src/com/mindtree/comicapp/exception/service/custom/SuperHeroAlreadyExistsException.java
@@ -0,0 +1,31 @@
+package com.mindtree.comicapp.exception.service.custom;
+
+import com.mindtree.comicapp.exception.service.ComicAppServiceException;
+
+public class SuperHeroAlreadyExistsException extends ComicAppServiceException {
+
+	public SuperHeroAlreadyExistsException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public SuperHeroAlreadyExistsException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public SuperHeroAlreadyExistsException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public SuperHeroAlreadyExistsException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public SuperHeroAlreadyExistsException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+}
