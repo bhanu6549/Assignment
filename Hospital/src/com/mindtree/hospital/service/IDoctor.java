d20bd6046ecebc322b6abc483af46e4f106a83ba
erpujabaidya <erpujabaidya@gmail.com>
2020-03-20 12:06:22 +0530

    Initiated by Puja

---
+++ Hospital/src/com/mindtree/hospital/service/IDoctor.java
@@ -0,0 +1,13 @@
+package com.mindtree.hospital.service;
+
+import com.mindtree.hospital.entity.Doctor;
+
+public interface IDoctor {
+
+	public Doctor [] displaySpecilazation(Doctor[] doctor);
+
+	public void setSize(int size);
+
+	//Doctor [] getDisplay(Doctor[] doctor);
+
+}
